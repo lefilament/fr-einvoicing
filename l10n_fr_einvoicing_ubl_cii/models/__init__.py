8e37f52debb72f09bda450ed4c8b7565dd74ab03
Rémi - Le Filament <30716308+remi-filament@users.noreply.github.com>
2026-06-08 18:45:39 +0200

    [ADD] import invoice

--- l10n_fr_einvoicing_ubl_cii/models/__init__.py
+++ l10n_fr_einvoicing_ubl_cii/models/__init__.py
@@ -1,3 +1,4 @@
 from . import account_edi_xml_cii_facturx
 from . import account_move
+from . import fr_einvoicing_flow
 from . import res_partner
---
+++ l10n_fr_einvoicing_ubl_cii/models/fr_einvoicing_flow.py
@@ -0,0 +1,40 @@
+# Copyright 2026 Le Filament (https://le-filament.com/)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+import base64
+import logging
+
+from odoo import models
+
+logger = logging.getLogger(__name__)
+
+
+class FrEinvoicingFlow(models.Model):
+    _inherit = "fr.einvoicing.flow"
+
+    def _import_supplier_invoice(self):
+        invoice_id = super()._import_supplier_invoice()
+        if not invoice_id:
+            attachment = self.env["ir.attachment"].create(
+                {
+                    "name": self.filename,
+                    "raw": base64.b64decode(self.file_bin),
+                    "type": "binary",
+                }
+            )
+            journal = self.env.ref(f"account.{self.env.company.id}_purchase")
+            move_type = "in_invoice"
+            invoice = self.env["account.move"].create(
+                {
+                    "journal_id": journal.id,
+                    "move_type": move_type,
+                    "fr_einvoicing_flow_id": self.id,
+                }
+            )
+            invoice._extend_with_attachments(attachment, new=True)
+            invoice._message_log(
+                body=self.env._("Facture reçue depuis la PA"),
+                attachment_ids=attachment.ids,
+            )
+            attachment.write({"res_model": "account.move", "res_id": invoice.id})
+            invoice_id = invoice.id
+        return invoice_id
