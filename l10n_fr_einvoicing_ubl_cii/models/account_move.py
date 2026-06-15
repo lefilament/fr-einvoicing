dc46a88e8092a458f7652638168deb69cc77cdc1
Rémi - Le Filament <30716308+remi-filament@users.noreply.github.com>
2026-06-15 18:28:14 +0200

    [IMP] adapt to origin l10n_fr_einvoicing changes

--- l10n_fr_einvoicing_ubl_cii/models/account_move.py
+++ l10n_fr_einvoicing_ubl_cii/models/account_move.py
@@ -1,47 +1,17 @@
 # Copyright 2026 Le Filament (https://le-filament.com/)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from odoo import models
-from odoo.exceptions import ValidationError
 
 
 class AccountMove(models.Model):
     _inherit = "account.move"
 
-    def _fr_ctc_send_invoice_prepare_flow_facturx(self):
-        self.ensure_one()
+    def _fr_ctc_prepare_flow(self):
+        # Extends l10n_fr_einvocing
+        vals = super()._fr_ctc_prepare_flow
+        # If invoice_edi_format set on partner is UBL 21 for France we update syntax
+        # Otherwise we keep default Factur-X
         invoice_edi_format = self.commercial_partner_id.invoice_edi_format
-        # If PDF and XML have not yet been generated, we force generation
-        if invoice_edi_format not in ["facturx", "ubl_21_fr"]:
-            raise ValidationError(
-                self.env._(
-                    "Your partner need to be configured with an EDI format "
-                    "supported by French Accredited Platforms (UBL 2.1 or Factur-x)"
-                )
-            )
-        if not self.invoice_pdf_report_id or not self.ubl_cii_xml_id:
-            self.env["account.move.send"]._generate_and_send_invoices(self)
-        vals = {
-            "processing_rule": "B2B",
-            "type": "CustomerInvoice",
-            "direction": "out",
-            "company_id": self.company_id.id,
-        }
-        if invoice_edi_format == "facturx" and self.invoice_pdf_report_id:
-            vals.update(
-                {
-                    "syntax": "Factur-X",
-                    "filename": self.invoice_pdf_report_id.name,
-                    "file_bin": self.invoice_pdf_report_id.datas,
-                }
-            )
-        elif invoice_edi_format == "ubl_21_fr" and self.ubl_cii_xml_id:
-            vals.update(
-                {
-                    "syntax": "UBL",
-                    "filename": self.ubl_cii_xml_id.name,
-                    "file_bin": self.ubl_cii_xml_id.datas,
-                }
-            )
-        else:
-            raise ValidationError(self.env._("Error when generating Invoice XML file"))
+        if invoice_edi_format == "ubl_21_fr":
+            vals.update({"syntax": "UBL"})
         return vals
--- l10n_fr_einvoicing_ubl_cii/models/fr_einvoicing_flow.py
+++ l10n_fr_einvoicing_ubl_cii/models/fr_einvoicing_flow.py
@@ -1,7 +1,15 @@
 # Copyright 2026 Le Filament (https://le-filament.com/)
+
+# For _match_partner function copied from l10n_fr_business_document_import:
+# Copyright 2015-2021 Akretion France (http://www.akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 import base64
 import logging
+
+from stdnum.fr.siren import is_valid as siren_is_valid
+from stdnum.fr.siret import is_valid as siret_is_valid
 
 from odoo import models
 
@@ -11,7 +19,26 @@ logger = logging.getLogger(__name__)
 class FrEinvoicingFlow(models.Model):
     _inherit = "fr.einvoicing.flow"
 
+    def _generate_invoice_file(self):
+        # Replace same function from l10n_fr_einvoicing to use account_edi_ubl_cii
+        # iso OCA modules for generating invoice Factur-x / UBL
+        self.ensure_one()
+        # If PDF and XML have not yet been generated, we force generation
+        if not self.move_id.invoice_pdf_report_id or not self.move_id.ubl_cii_xml_id:
+            self.env["account.move.send"]._generate_and_send_invoices(self.move_id)
+        if self.syntax == "Factur-X" and self.move_id.invoice_pdf_report_id:
+            extension = "pdf"
+            file_b64 = self.move_id.invoice_pdf_report_id.datas
+        elif self.syntax == "UBL" and self.move_id.ubl_cii_xml_id:
+            extension = "xml"
+            file_b64 = self.move_id.ubl_cii_xml_id.datas
+        if extension and file_b64:
+            return extension, file_b64
+        else:
+            return super()._generate_invoice_file()
+
     def _import_supplier_invoice(self):
+        # extends from l10n_fr_einvoicing
         invoice_id = super()._import_supplier_invoice()
         if not invoice_id:
             attachment = self.env["ir.attachment"].create(
@@ -36,3 +63,37 @@ class FrEinvoicingFlow(models.Model):
                 attachment_ids=attachment.ids,
             )
         return invoice.id
+
+    def _match_partner(self, partner_dict, chatter_msg):
+        # Replace same method from l10n_fr_einvoicing to not depend
+        # on base.document.import
+        # Copy method from l10n_fr_business_document_import
+        # Since here partner_dict contains only SIREN or SIRET
+        if partner_dict.get("siret"):
+            siret = partner_dict["siret"].replace(" ", "")
+            if siret_is_valid(siret):
+                partner = self.env["res.partner"].search(
+                    [("siret", "=", siret)], limit=1
+                )
+                if partner:
+                    return partner
+                # fallback on siren search
+                elif not partner_dict.get("siren"):
+                    partner_dict["siren"] = siret[:9]
+        if partner_dict.get("siren"):
+            # when partner_dict comes from invoice2data, siren may be an int
+            if isinstance(partner_dict["siren"], int):
+                siren = str(partner_dict["siren"])
+            else:
+                siren = partner_dict["siren"].replace(" ", "")
+            if siren_is_valid(siren):
+                partner = self.env["res.partner"].search(
+                    [
+                        ("parent_id", "=", False),
+                        ("siren", "=", siren),
+                    ],
+                    limit=1,
+                )
+                if partner:
+                    return partner
+        return False
