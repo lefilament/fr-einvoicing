df7a4ba233d25b6653821659bd5c27445724b2ca
Rémi - Le Filament <30716308+remi-filament@users.noreply.github.com>
2026-08-19 23:07:47 +0200

    [ADD] missing python libraries dependencies

--- account_invoice_en16931/__manifest__.py
+++ account_invoice_en16931/__manifest__.py
@@ -19,7 +19,7 @@
         "intrastat_base",
     ],
     "excludes": ["account_einvoice_generate"],
-    "external_dependencies": {"python": ["factur-x>=6.8"]},
+    "external_dependencies": {"python": ["factur-x>=6.8", "packaging"]},
     "data": [
         "security/ir.model.access.csv",
         "wizards/account_invoice_en16931_generate_view.xml",
